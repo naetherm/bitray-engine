6526584231a28527759094e60519c3c0a5dcaa91
naetherm <naether.markus@gmail.com>
2025-01-03 16:37:00 +0100

    feat: add StringSplitter implementation

---
+++ engine/foundation/core/public/core/string/string_splitter.h
@@ -0,0 +1,89 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2024 RacoonStudios
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this
+// software and associated documentation files (the "Software"), to deal in the Software
+// without restriction, including without limitation the rights to use, copy, modify, merge,
+// publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons
+// to whom the Software is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all copies or
+// substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+// INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+// PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE
+// FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+// OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+// DEALINGS IN THE SOFTWARE.
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+//[-------------------------------------------------------]
+//[ Header guard                                          ]
+//[-------------------------------------------------------]
+#pragma once
+
+
+//[-------------------------------------------------------]
+//[ Includes                                              ]
+//[-------------------------------------------------------]
+#include "core/core.h"
+#include "core/container/vector.h"
+#include "core/string/string.h"
+
+
+//[-------------------------------------------------------]
+//[ Namespace                                             ]
+//[-------------------------------------------------------]
+namespace core {
+
+
+//[-------------------------------------------------------]
+//[ Classes                                               ]
+//[-------------------------------------------------------]
+/**
+ * @class
+ * BasicStringSplitter
+ *
+ * @brief
+ * A utility class for splitting strings based on a specified delimiter.
+ * Designed to work with `BasicString` objects of any character type.
+ *
+ * @tparam TType The character type used in the `BasicString` (e.g., char, wchar_t).
+ */
+template<typename TType>
+class BasicStringSplitter {
+public:
+
+  /**
+   * @brief
+   * Splits a string into a vector of substrings.
+   *
+   * Divides the given string `str` into substrings using `splitter` as the
+   * delimiter. The resulting substrings are returned in a vector.
+   *
+   * @param[in] str The input string to be split.
+   * @param[in] splitter The delimiter string used to divide `str`.
+
+   * @return A vector containing the substrings resulting from the split.
+   */
+  static Vector<BasicString<TType>> split(const BasicString<TType> & str, const BasicString<TType>& splitter);
+
+private:
+};
+
+
+typedef BasicStringSplitter<char> StringSplitter;
+
+
+//[-------------------------------------------------------]
+//[ Namespace                                             ]
+//[-------------------------------------------------------]
+} // core
+
+
+//[-------------------------------------------------------]
+//[ Implementation                                        ]
+//[-------------------------------------------------------]
+#include "core/string/string_splitter.inl"
---
+++ engine/foundation/core/public/core/string/string_splitter.inl
@@ -0,0 +1,53 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2024 RacoonStudios
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this
+// software and associated documentation files (the "Software"), to deal in the Software
+// without restriction, including without limitation the rights to use, copy, modify, merge,
+// publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons
+// to whom the Software is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all copies or
+// substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+// INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+// PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE
+// FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+// OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+// DEALINGS IN THE SOFTWARE.
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+//[-------------------------------------------------------]
+//[ Includes                                              ]
+//[-------------------------------------------------------]
+#include "core/string/tokenizer.h"
+
+
+//[-------------------------------------------------------]
+//[ Namespace                                             ]
+//[-------------------------------------------------------]
+namespace core {
+
+
+//[-------------------------------------------------------]
+//[ Classes                                               ]
+//[-------------------------------------------------------]
+template<typename TType>
+Vector<BasicString<TType>> BasicStringSplitter<TType>::split(const BasicString<TType> & str, const BasicString<TType>& splitter) {
+  BasicTokenizer<TType> tokenizer;
+  tokenizer.set_delimiters(splitter);
+  tokenizer.set_single_line_comment("");
+  tokenizer.set_quotes("");
+  tokenizer.set_single_chars("");
+  tokenizer.start(str);
+
+  return tokenizer.get_tokens();
+}
+
+
+//[-------------------------------------------------------]
+//[ Namespace                                             ]
+//[-------------------------------------------------------]
+} // core
