b05e39fb3a41c9484303d9c83574f88a75fc3475
naetherm <naether.markus@gmail.com>
2025-01-11 08:11:51 +0100

    feat: add vulkan-sdk to download_dependencies.sh

--- scripts/download_dependencies.sh
+++ scripts/download_dependencies.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 mkdir -p "./.bitray_engine/Externals/"
+EXTERNALS_DIR="./.bitray_engine/Externals"
 
 _deps=(
   "glm-0.9.9.8-rev1-linux"
@@ -12,7 +13,34 @@ _deps=(
 
 for dep in ${_deps[*]}
 do
-  wget "http://79.143.191.61/re/${dep}.tar.xz" -P "./.bitray_engine/Externals/"
-  mkdir -p "./.bitray_engine/Externals/${dep}"
-  tar -xf "./.bitray_engine/Externals/${dep}.tar.xz" -C "./.bitray_engine/Externals/${dep}"
+  if [ ! -f "${EXTERNALS_DIR}/${dep}.tar.xz" ]
+  then
+    wget "http://79.143.191.61/re/${dep}.tar.xz" -P ${EXTERNALS_DIR}
+    mkdir -p "${EXTERNALS_DIR}/${dep}"
+
+    if [ ! -f "${EXTERNALS_DIR}/${dep}" ]
+    then
+      tar -xf "${EXTERNALS_DIR}/${dep}.tar.xz" -C "./.bitray_engine/Externals/${dep}"
+    fi
+  else
+    echo "${dep}.tar.xz already present, skipping ... "
+  fi
 done
+
+# Download the LunarG Vulkan SDK to the Externals directory as well
+VULKAN_SDK_VERSION="1.3.231.2"
+#VULKAN_SDK_VERSION="1.2.131.2"
+VULKAN_SDK_PATH="https://sdk.lunarg.com/sdk/download/${VULKAN_SDK_VERSION}/linux/vulkansdk-linux-x86_64-${VULKAN_SDK_VERSION}.tar.gz"
+
+if [ ! -f "${EXTERNALS_DIR}/vulkansdk-linux-x86_64-${VULKAN_SDK_VERSION}.tar.gz" ]
+then
+  DEP="vulkansdk-linux-x86_64-${VULKAN_SDK_VERSION}"
+  wget ${VULKAN_SDK_PATH} -P ${EXTERNALS_DIR}
+fi
+
+if [ ! -f "${EXTERNALS_DIR}/vulkansdk-linux" ]
+then
+  DEP="vulkansdk-linux-x86_64-${VULKAN_SDK_VERSION}"
+  mkdir -p "${EXTERNALS_DIR}/vulkansdk-linux"
+  tar -xf "${EXTERNALS_DIR}/${DEP}.tar.gz" -C "./.bitray_engine/Externals/vulkansdk-linux"
+fi
