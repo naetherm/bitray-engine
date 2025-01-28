f161140b1e53722093807a94cf47c513df484d85
naetherm <naether.markus@gmail.com>
2025-01-28 06:28:06 +0100

    feat: add sample enum to test

--- engine/tests/rtti_tests/public/some_object.h
+++ engine/tests/rtti_tests/public/some_object.h
@@ -80,6 +80,12 @@ public:
   ~SomeObject() override;
 };
 
+enum SomeEnum {
+  FirstValue = 0,
+  SecondValue = 1,
+  ThirdValue = 2,
+};
+
 
 //[-------------------------------------------------------]
 //[ Namespace                                             ]
